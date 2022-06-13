818e46d762178d2b6fa5a3569e5729716e104220
Kalugy <danieltrujillo901@gmail.com>
2022-06-13 11:38:45 -0500

    router between links

--- src/Router/RouterNav.js
+++ src/Router/RouterNav.js
@@ -24,21 +24,19 @@ function RouterNav() {
     <div>
     <BrowserRouter>
       <nav style={test}>
-        <Link to="/" style={stylesVersions}>Version1</Link>
-        <Link to="test" style={stylesVersions}>Version2</Link>
-        <Link to="test2" style={stylesVersions}>Version3</Link>
-        <Link to="test3" style={stylesVersions}>Version4</Link>
+        <Link to="kalugy-projects/" style={stylesVersions}>Version1</Link>
+        <Link to="kalugy-projects/v2" style={stylesVersions}>Version2</Link>
+        <Link to="kalugy-projects/v3" style={stylesVersions}>Version3</Link>
+        <Link to="kalugy-projects/v4" style={stylesVersions}>Version4</Link>
       </nav>
       <Routes>
-        <Route path="/" element={<Main/>} />
-        <Route path="test" element={<Option2 />} />
-        <Route path="test2" element={<h1>version3</h1>} />
-        <Route path="test3" element={<h1>version4</h1>} />
-        <Route path="test4" element={<h1>version5</h1>} />
+        <Route path="kalugy-projects/" element={<Main/>} />
+        <Route path="kalugy-projects/v2" element={<Option2 />} />
+        <Route path="kalugy-projects/v3" element={<h1>version3</h1>} />
+        <Route path="kalugy-projects/v4" element={<h1>version4</h1>} />
       </Routes>
     </BrowserRouter>
     </div>
   );
 }
-
 export default RouterNav;
