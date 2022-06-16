06d6f32c23f1fd39839b21bcbb2d146efe39fb38
Kalugy <danieltrujillo901@gmail.com>
2022-06-16 11:36:10 -0500

    fixing menu main navbar

--- src/Router/RouterNav.js
+++ src/Router/RouterNav.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
   BrowserRouter as Router,
   Routes,
@@ -15,7 +15,9 @@ import './RouterNav.css'
 
 const stylesVersions={
   color:"white",
-  margin: "2px"
+  fontSize:"1rem",
+  fontWeight:"600"
+
 }
 
 const paddingTest={
@@ -23,17 +25,45 @@ const paddingTest={
 }
 
 function RouterNav() {
+  useEffect(()=>{
+    console.log("jere")
+    document.getElementById('myButton').onclick = function() {
+      let menu = document.querySelector(".nav-main-items")
+      
+      menu.classList.toggle('active-navbar-main-trans');
+    }
+    /*
+    let menuButton = document.querySelector(".navbar-icon-con")
+    console.log("men", menuButton)
+    menuButton.addEventListener("click",()=>{
+      console.log("CLICK")
+      let menu = document.querySelector(".nav-main-items")
+      menu.classList.toggle('active-navbar-main-trans')
+    })*/
+  })
+
   return (
     <div>
     <BrowserRouter>
-    
-      <input type="checkbox" id="collider-button-nav"></input>
-      <nav className="nav-test">
-        <Link to="kalugy-projects/" style={stylesVersions}>Version1</Link>
-        <Link to="kalugy-projects/v2" style={stylesVersions}>Version2</Link>
-        <Link to="kalugy-projects/v3" style={stylesVersions}>Version3</Link>
-        <Link to="kalugy-projects/v4" style={stylesVersions}>Version4</Link>
-      </nav>
+      <div className="menu-navbar-main">
+        <div className="navbar-icon-con" id="myButton">
+          <i className="fa-solid fa-bars"></i>
+        </div>
+        <nav className="nav-main-items">
+          <div className="nav-main-item">
+            <Link to="kalugy-projects/" style={stylesVersions}>Version1</Link>
+          </div>
+          <div className="nav-main-item">
+            <Link to="kalugy-projects/v2" style={stylesVersions}>Version2</Link>
+          </div>
+          <div className="nav-main-item">
+            <Link to="kalugy-projects/v3" style={stylesVersions}>Version3</Link>
+          </div>
+          <div className="nav-main-item">
+            <Link to="kalugy-projects/v4" style={stylesVersions}>Version4</Link>
+          </div>
+        </nav>
+      </div>
       <Routes>
         <Route path="kalugy-projects/" element={<Main/>} />
         <Route path="kalugy-projects/v2" element={<Option2 />} />
