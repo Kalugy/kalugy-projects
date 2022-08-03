016ad3099ea57ed7c24319b095ed15f1fb5bfc0c
Kalugy <danieltrujillo901@gmail.com>
2022-08-02 20:07:40 -0500

    test

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     //<App/>
-    <div>HOLA</div>
+    <React.StrictMode>
+        <div>HOLA</div>
+    </React.StrictMode>
 );
 {/*<React.StrictMode></React.StrictMode>*/}
 // If you want to start measuring performance in your app, pass a function
